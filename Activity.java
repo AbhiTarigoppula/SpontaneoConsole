03810492fa3b5fd12351cff998503aa30403a892
Abhi Tarigoppula <tvkabhiram@gmail.com>
2026-03-08 19:52:45 -0700

    Set methods up for continuing work

--- Activity.java
+++ Activity.java
@@ -1,3 +1,51 @@
 public class Activity {
+    private String name;
+    private String budget;
+    private String locationType;
+    private String vibe;
+
+    public Activity (String name, String budget, String locationType, String vibe) {
+        this.name = name;
+        this.budget = budget;
+        this.locationType = locationType;
+        this.vibe = vibe;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    public String getLocationType() {
+        return locationType;
+    }
+
+    public void setLocationType(String locationType) {
+        this.locationType = locationType;
+    }
+
+    public String getVibe() {
+        return vibe;
+    }
+
+    public void setVibe(String vibe) {
+        this.vibe = vibe;
+    }    
+
+    @Override
+    public String toString() {
+        return "";
+    }
 
 }
--- Spontaneo.java
+++ Spontaneo.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Spontaneo {    
     public static void main(String[] args) {
-        System.out.println("Hello World");
+    
+       List<Activity> activities = new ArrayList<>();
+
+
+              
+    }
+
+    // Adding activities to the list
+    public static void addActivities(List<Activity> activities) {
+
+    }
+
+
+
+    // Asking user for city
+    public static String askCity(Scanner console) {
+        return "";
+    }
+
+    // Asking user for budget
+    public static String askBudget(Scanner console) {
+        return "";
+    }
+
+    // Asking user for location type
+    public static String askLocation(Scanner console) {
+        return "";
+    }    
+
+    // Asking user for vibe
+    public static String askVibe(Scanner console) {
+        return "";
+    }    
+
+
+    public static List<Activity> findMatches(List<Activity> activities, String budget, String locationType, String vibe) {
+        List<Activity> list = new ArrayList<>();
+        return list;
+    }
+
+
+    // Bonus Challenges
+    public static void bonus(Scanner console) {
+
     }
 }
